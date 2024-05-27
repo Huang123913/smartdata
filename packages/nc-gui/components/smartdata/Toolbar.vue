bf3477fa71593b9c5cdae43edbf1ec2555077310
chavyleung <chavyleung@gmail.com>
2024-05-27 17:27:54 +0800

    chore(nc-gui): 优化演练场顶部导航条样式

--- packages/nc-gui/components/smartdata/Toolbar.vue
+++ packages/nc-gui/components/smartdata/Toolbar.vue
@@ -3,6 +3,8 @@ const { isMobileMode } = useGlobal()
 const { baseUrl } = useBase()
 
 const { base, isSharedBase } = storeToRefs(useBase())
+const { isLeftSidebarOpen } = storeToRefs(useSidebarStore())
+const { activeTable } = storeToRefs(useTablesStore())
 
 const openedBaseUrl = computed(() => {
   if (!base.value) return ''
@@ -17,49 +19,72 @@ const openedBaseUrl = computed(() => {
 
 <template>
   <div
-    class="flex flex-row pl-2 pr-2 gap-1 border-b-1 border-gray-200 justify-between w-full"
-    :class="{ 'nc-table-toolbar-mobile': isMobileMode, 'h-[var(--topbar-height)]': !isMobileMode }"
+    class="nc-table-topbar h-20 py-1 flex gap-2 items-center border-b border-gray-200 overflow-hidden relative max-h-[var(--topbar-height)] min-h-[var(--topbar-height)] md:(pr-2 pl-2) xs:(px-1)"
+    style="z-index: 7"
   >
-    <div class="flex flex-row items-center gap-x-3">
-      <GeneralOpenLeftSidebarBtn />
-      <div class="ml-0.25 flex flex-row font-medium items-center border-gray-50 transition-all duration-100">
-        <template v-if="!isMobileMode">
-          <NuxtLink class="!hover:(text-black underline-gray-600) !underline-transparent ml-0.75 max-w-1/4" :to="openedBaseUrl">
-            <NcTooltip class="!text-inherit">
-              <template #title>
-                <span class="capitalize">
+    <GeneralOpenLeftSidebarBtn />
+    <div
+      class="ml-0.25 flex flex-row font-medium items-center border-gray-50 transition-all duration-100"
+      :class="{
+        'min-w-36/100 max-w-36/100': !isMobileMode && isLeftSidebarOpen,
+        'min-w-39/100 max-w-39/100': !isMobileMode && !isLeftSidebarOpen,
+        'w-2/3 text-base ml-1.5': isMobileMode,
+        '!max-w-3/4': isSharedBase && !isMobileMode,
+      }"
+    >
+      <template v-if="!isMobileMode">
+        <NuxtLink
+          class="!hover:(text-black underline-gray-600) !underline-transparent ml-0.75 max-w-1/4"
+          :class="{
+            '!max-w-none': isSharedBase && !isMobileMode,
+            '!text-gray-500': activeTable,
+            '!text-gray-700': !activeTable,
+          }"
+          :to="openedBaseUrl"
+        >
+          <NcTooltip class="!text-inherit">
+            <template #title>
+              <span class="capitalize">
+                {{ base?.title }}
+              </span>
+            </template>
+            <div class="flex flex-row items-center gap-x-1.5">
+              <GeneralProjectIcon
+                :type="base?.type"
+                class="!grayscale min-w-4"
+                :style="{
+                  filter: 'grayscale(100%) brightness(115%)',
+                }"
+              />
+              <div
+                class="!2xl:(flex truncate ml-1)"
+                :class="{
+                  '!flex': isSharedBase && !isMobileMode,
+                }"
+              >
+                <span class="truncate !text-inherit capitalize">
                   {{ base?.title }}
                 </span>
-              </template>
-              <div class="flex flex-row items-center gap-x-1.5">
-                <GeneralProjectIcon
-                  :type="base?.type"
-                  class="!grayscale min-w-4"
-                  :style="{
-                    filter: 'grayscale(100%) brightness(115%)',
-                  }"
-                />
-                <div class="hidden !2xl:(flex truncate ml-1)">
-                  <span class="truncate !text-inherit capitalize">
-                    {{ base?.title }}
-                  </span>
-                </div>
               </div>
-            </NcTooltip>
-          </NuxtLink>
-          <div class="px-1.75 text-gray-500">/</div>
-        </template>
-        <template v-if="!isMobileMode">
-          <NcTooltip class="truncate nc-active-table-title max-w-full" show-on-truncate-only>
-            <template #title>
-              {{ $t('placeholder.createTableLabel') }}
-            </template>
-            <span class="text-ellipsis overflow-hidden text-gray-500 xs:ml-2 text-gray-500">
-              {{ $t('title.creatingTable') }}
-            </span>
+            </div>
           </NcTooltip>
-        </template>
-      </div>
+        </NuxtLink>
+        <div class="px-1.75 text-gray-500">></div>
+      </template>
+      <template v-if="!isMobileMode">
+        <NcTooltip class="truncate nc-active-table-title max-w-full" show-on-truncate-only>
+          <template #title>
+            {{ $t('placeholder.createTableLabel') }}
+          </template>
+          <span
+            :class="{
+              'max-w-28/100': !isMobileMode,
+            }"
+          >
+            {{ $t('title.creatingTable') }}
+          </span>
+        </NcTooltip>
+      </template>
     </div>
   </div>
 </template>
