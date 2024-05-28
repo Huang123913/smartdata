adf34354b5c99d18ccb7f892af5001333f6c467c
chavyleung <chavyleung@gmail.com>
2024-05-28 17:14:38 +0800

    feat(nocodb): 对接排序接口

--- packages/nocodb/src/controllers/sorts.controller.ts
+++ packages/nocodb/src/controllers/sorts.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Req,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { SortReqType } from 'nocodb-sdk';
 import { GlobalGuard } from '~/guards/global/global.guard';
@@ -17,6 +18,8 @@ import { SortsService } from '~/services/sorts.service';
 import { Acl } from '~/middlewares/extract-ids/extract-ids.middleware';
 import { MetaApiLimiterGuard } from '~/guards/meta-api-limiter.guard';
 import { NcRequest } from '~/interface/config';
+
+import { MCDMRewrite } from '~/modules/smartdata/interceptors/MCDMInterceptor';
 
 @Controller()
 @UseGuards(MetaApiLimiterGuard, GlobalGuard)
@@ -28,6 +31,7 @@ export class SortsController {
     '/api/v2/meta/views/:viewId/sorts/',
   ])
   @Acl('sortList')
+  @UseInterceptors(MCDMRewrite('NocodbDBTableSortListViewSorts'))
   async sortList(@Param('viewId') viewId: string) {
     return new PagedResponseImpl(
       await this.sortsService.sortList({
@@ -42,6 +46,7 @@ export class SortsController {
   ])
   @HttpCode(200)
   @Acl('sortCreate')
+  @UseInterceptors(MCDMRewrite('NocodbDBTableSortUpdateViewSort'))
   async sortCreate(
     @Param('viewId') viewId: string,
     @Body() body: SortReqType,
@@ -57,6 +62,7 @@ export class SortsController {
 
   @Get(['/api/v1/db/meta/sorts/:sortId', '/api/v2/meta/sorts/:sortId'])
   @Acl('sortGet')
+  @UseInterceptors(MCDMRewrite('NocodbDBTableSortGetSort'))
   async sortGet(@Param('sortId') sortId: string) {
     const sort = await this.sortsService.sortGet({
       sortId,
@@ -66,6 +72,7 @@ export class SortsController {
 
   @Patch(['/api/v1/db/meta/sorts/:sortId', '/api/v2/meta/sorts/:sortId'])
   @Acl('sortUpdate')
+  @UseInterceptors(MCDMRewrite('NocodbDBTableSortUpdateSort'))
   async sortUpdate(
     @Param('sortId') sortId: string,
     @Body() body: SortReqType,
@@ -81,6 +88,7 @@ export class SortsController {
 
   @Delete(['/api/v1/db/meta/sorts/:sortId', '/api/v2/meta/sorts/:sortId'])
   @Acl('sortDelete')
+  @UseInterceptors(MCDMRewrite('NocodbDBTableSortDeleteSort'))
   async sortDelete(@Param('sortId') sortId: string, @Req() req: NcRequest) {
     const sort = await this.sortsService.sortDelete({
       sortId,
